8c35ee9b8c8c4285592b65301b5133610b181296
fpolidoro <fpolidoro@users.noreply.github.com>
2023-11-19 18:04:27 +0800

    Connect slider to play button and animate
    - Click on play animates the image and updates the position of the slider
    - [to do] Connect undo action and stop button to animation
    - [to do] Implement pause

--- src/app/preview/components/preview-box/preview-box.component.ts
+++ src/app/preview/components/preview-box/preview-box.component.ts
@@ -1,16 +1,16 @@
-import { Component, HostListener, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { Form, FormArray, FormControl, FormGroup } from '@angular/forms';
 import { IImageFile, ISize, IViewport } from '@preview/interfaces/files';
 import { IArea, ITile } from '@preview/interfaces/shapes';
 import { FileService, IAreaDragged } from '@preview/services/file.service';
-import { Observable, Subject, combineLatest, filter, map, merge, startWith, switchMap, tap, withLatestFrom } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, combineLatest, filter, interval, map, merge, startWith, switchMap, take, takeUntil, tap, withLatestFrom } from 'rxjs';
 
 @Component({
   selector: 'preview-box',
   templateUrl: './preview-box.component.html',
   styleUrls: ['./preview-box.component.scss']
 })
-export class PreviewBoxComponent implements OnInit {
+export class PreviewBoxComponent implements OnInit, OnDestroy {
 
   viewportSize: ISize = { width: 0, height: 0 }
   tile: ITile = { c: -1, r: -1 }
@@ -20,9 +20,12 @@ export class PreviewBoxComponent implements OnInit {
   image?: IImageFile
   originalViewport: IViewport = { cols: 0, rows: 0}
   selectedAreaForm?: FormGroup
+  
+  playPause$: Subject<void> = new Subject<void>()
+  isPlaying: boolean = false
 
   /** Keeps track the current position of the slider during slider drag (valueChanges in a FormControl would only update when the user releases the slider) */
-  playPosition$: Subject<any> = new Subject<any>()
+  playPosition$: Subject<number> = new Subject<number>()
   /** Observable to update the `background-position` CSS style of the preview, so that it animates according to the position of the slider*/
   backgroundPosition$: Observable<ITile> = this.playPosition$.pipe(
     withLatestFrom(this._fileService.selectedInteractiveAreaChanged$.pipe(
@@ -30,24 +33,24 @@ export class PreviewBoxComponent implements OnInit {
         map((formArray: FormArray) => formArray.controls[selectedArea] as FormGroup)  //take the form of the currently selected area because we'll need the direction later
       ))
     )),
-    map(([positionEvent, areaForm]) => {  //compute the background position so it can be retrieved by the async pipe in the template
+    map(([position, areaForm]) => {  //compute the background position so it can be retrieved by the async pipe in the template
       let cssBackgroundPosition: ITile = { c: 0, r: 0}
       switch(areaForm.controls['direction'].value.label){
         case 'Row': //update the background-position-x value
-          cssBackgroundPosition.c = -this.viewportSize.width*positionEvent.value
+          cssBackgroundPosition.c = -this.viewportSize.width*position
           break
         case 'Column': //update the background-position-x value
-          cssBackgroundPosition.r = -this.viewportSize.height*positionEvent.value
+          cssBackgroundPosition.r = -this.viewportSize.height*position
           break
       }
-
-      //console.log(cssBackgroundPosition)
+      console.log(cssBackgroundPosition)
       return cssBackgroundPosition
     })
   )
   
   private _areas: IArea[] = []
   private _windowSize$: Subject<ISize> = new Subject<ISize>()
+  private _onDestroy$: Subject<void> = new Subject<void>()
   
   constructor(private _fileService: FileService) { }
 
@@ -65,7 +68,9 @@ export class PreviewBoxComponent implements OnInit {
           this.panelWidth = (size.width*0.2) - 24
         })
       )
-    ]).subscribe(([file, viewport, size]) => {
+    ]).pipe(
+      takeUntil(this._onDestroy$)
+    ).subscribe(([file, viewport, size]) => {
       this.originalViewport = viewport
       let w = file.width/(viewport.cols > 0 ? viewport.cols : 1)
       let h = file.height/(viewport.rows > 0 ? viewport.rows : 1)
@@ -95,12 +100,31 @@ export class PreviewBoxComponent implements OnInit {
           tap((formArray: FormArray) => {
             this.selectedAreaForm = formArray.controls[selectedArea] as FormGroup
             this.tile = this._areas[selectedArea].pos
-          })
+          }),
+          map((formArray: FormArray) => formArray.controls[selectedArea] as FormGroup),
+          switchMap((formGroup: FormGroup) => this.playPause$.pipe( //listen to the play/pause button
+            tap(() => this.isPlaying = !this.isPlaying),  //toggle the icon for the template
+            tap(() => console.log(`play ${1+Math.abs(formGroup.controls['to'].value-formGroup.controls['from'].value)} frames`)),
+            switchMap(() => interval(150).pipe( //emit a value every 150ms that updates the slider and emits a value to update the background-position
+              tap((position: number) => {
+                console.log(`Emitting ${position}`)
+                this.playPosition$.next(position)
+              }),
+              take(Math.abs(1+formGroup.controls['to'].value-formGroup.controls['from'].value)),  //take until the number of frames to play is reached
+            )),
+          ))
         ))
       ),
+    ).pipe(
+      takeUntil(this._onDestroy$)
     ).subscribe()
     
     this.onResize(null)
+  }
+
+  ngOnDestroy(): void {
+    this._onDestroy$.next()
+    this._onDestroy$.complete()
   }
 
   @HostListener('window:resize', ['$event'])
