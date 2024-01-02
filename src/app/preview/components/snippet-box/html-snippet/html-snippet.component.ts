081bb683a1ac131203bceb2d4ba085afaab8026f
fpolidoro <fpolidoro@users.noreply.github.com>
2024-01-02 21:07:51 +0800

    Generate divs for interactive areas

--- src/app/preview/components/snippet-box/html-snippet/html-snippet.component.ts
+++ src/app/preview/components/snippet-box/html-snippet/html-snippet.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormArray } from '@angular/forms';
+import { FileService } from '@preview/services/file.service';
+import { Observable, Subject, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'html-snippet',
   templateUrl: './html-snippet.component.html',
   styleUrls: ['./html-snippet.component.scss']
 })
-export class HtmlSnippetComponent implements OnInit {
+export class HtmlSnippetComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  areas: AbstractControl[] = []
+
+  private _onDestroy$: Subject<void> = new Subject<void>()
+
+  constructor(private _fileService: FileService) { }
 
   ngOnInit(): void {
+    this._fileService.formArray$.pipe(
+      tap((formArray: FormArray) => this.areas = formArray.controls),
+      takeUntil(this._onDestroy$)
+      ).subscribe()
   }
 
+  ngOnDestroy(): void {
+    this._onDestroy$.next()
+    this._onDestroy$.complete()
+  }
 }
