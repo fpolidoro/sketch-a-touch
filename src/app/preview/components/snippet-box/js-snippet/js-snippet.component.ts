27fb7498e2ab06ce71934580fc1e6256dbd1f6c6
fpolidoro <fpolidoro@users.noreply.github.com>
2024-01-03 20:48:02 +0800

    Set up JS template
    - JS snippet handles click, direction and start-end frame for each interactive area
    - [not handled] Gestures other than tap, image location (local or online), script used locally or with online tools

--- src/app/preview/components/snippet-box/js-snippet/js-snippet.component.ts
+++ src/app/preview/components/snippet-box/js-snippet/js-snippet.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormGroup } from '@angular/forms';
+import { ISize } from '@preview/interfaces/files';
+import { IArea } from '@preview/interfaces/shapes';
+import { FileService } from '@preview/services/file.service';
+import { Subject, merge, tap, combineLatest, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'js-snippet',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./js-snippet.component.scss']
 })
 export class JsSnippetComponent implements OnInit {
+  areas: IArea[] = []
+  array: FormGroup[] = []
+  formArray?: FormArray
+  viewportSize: ISize = {width: 0, height: 0}
 
-  constructor() { }
+  private _onDestroy$: Subject<void> = new Subject<void>()
+
+  constructor(private _fileService: FileService) { }
 
   ngOnInit(): void {
+    merge(  //observe the changes to interactive areas and store them into an array: these areas will be used to generate the CSS code
+      this._fileService.interactiveAreaAnnounced$.pipe(
+        tap((area: IArea) => this.areas.push(area)),
+        tap(area => console.log(area))
+      ),
+      this._fileService.interactiveAreaDeleted$.pipe(
+        tap((index: number) => this.areas.splice(index, 1))
+      ),
+      combineLatest([
+        this._fileService.fileUploaded$,
+        this._fileService.viewportChanged$
+      ]).pipe(
+        tap(([file, viewportSize]) => this.viewportSize = {width: file.width/viewportSize.cols, height: file.height/viewportSize.rows}),
+      ),
+      this._fileService.formArray$.pipe(
+        tap((formArray) => {
+          this.formArray = formArray
+          this.array = formArray.controls as FormGroup[]
+        })
+      )
+    ).pipe(
+      takeUntil(this._onDestroy$)
+    ).subscribe()
   }
 
+  ngOnDestroy(): void {
+    this._onDestroy$.next()
+    this._onDestroy$.complete()
+  }
 }
